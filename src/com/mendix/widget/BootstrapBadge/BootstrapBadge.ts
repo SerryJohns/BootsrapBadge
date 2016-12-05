13044f44d91e0bb9dd9f08faee9cf28e2c6fe676
Andries Smit <mr.andries.smit@gmail.com>
2016-12-05 12:32:38 +0100

    Add inline review comments

--- src/com/mendix/widget/BootstrapBadge/BootstrapBadge.ts
+++ src/com/mendix/widget/BootstrapBadge/BootstrapBadge.ts
@@ -5,6 +5,7 @@ import { createElement } from "react";
 import { render } from "react-dom";
 
 import { BadgeComponent } from "./components/Badgelable";
+// Does not make your code better, you can ignore the warning...
 // tslint:disable-next-line:ordered-imports
 import { OnClickProps, BadgeType } from "./components/Badge";
 
@@ -16,14 +17,15 @@ class BootstrapBadge extends WidgetBase {
     private badgeType: BadgeType;
     private label: string;
     private badgeClass: string;
-    private onclickMicroflow: string;
+    private onclickMicroflow: string; // AS: next white line in no need.
 
-    // internal variables
+    // internal variables // Comments should start with a capital.
     private contextObject: mendix.lib.MxObject;
     private handles: number[];
     private badgeValue: string;
 
     createOnClickProps(): OnClickProps {
+        // The return and open object literal can start on one line. 
         return (
             {
                 applyto: "selection",
@@ -42,12 +44,12 @@ class BootstrapBadge extends WidgetBase {
     update(object: mendix.lib.MxObject, callback?: Function) {
         this.contextObject = object;
         this.resetSubscriptions();
-        this.updateRendering();
+        this.updateRendering(); // Add white line before function return or callback
         callback();
     }
 
     unsubscribe() {
-        if (this.handles) {
+        if (this.handles) { // no need to check handles, when initial value of is an empty array, as the for loop for not run and won't give an exception.
             for (let handle of this.handles) {
                 mx.data.unsubscribe(handle);
             }
@@ -55,9 +57,13 @@ class BootstrapBadge extends WidgetBase {
         }
     }
 
-    private updateRendering() {
+    private updateRendering() { // Ternary operator breaking lines should start newline with ? and : (not at the end of the line)
+        // let x = conditionStatement
+        //      ? value
+        //      : elseValue; 
         let attrStyle = this.contextObject ?
             this.getValue(this.contextObject.get(this.attrStyle) as string, this.badgeClass) : this.badgeClass;
+        // Use const not let, when variables do not change.
         let attrLabel = this.contextObject ?
             this.getValue(this.contextObject.get(this.attrLabel) as string, this.label) : this.label;
         this.badgeValue = this.contextObject ?
@@ -70,10 +76,10 @@ class BootstrapBadge extends WidgetBase {
             bootstrapStyle: attrStyle,
             label: attrLabel
         }), this.domNode
-        );
+        ); // The indent is wrong. It can move this parentheses one line up.
     }
-
-    private getValue(attr: string, otherValue: string) {
+    // Why not take in context as well? It would cleanup your updateRendering greatly
+    private getValue(attr: string, otherValue: string) { // Why is this var name named otherValue?
         return attr ? attr : otherValue;
     }
 
@@ -85,36 +91,38 @@ class BootstrapBadge extends WidgetBase {
                 callback: (guid) => this.updateRendering(),
                 guid: this.contextObject.getGuid()
             })
-            );
+            ); // The indent is wrong. It can move this parentheses one line up.
             this.handles.push( mx.data.subscribe({
                 attr: this.attrValue,
                 callback: ( guid, attr, badgeValue ) => this.updateRendering(),
                 guid: this.contextObject.getGuid()
             })
-            );
+            ); // The indent is wrong. It can move this parentheses one line up. // No need for white line... 
 
             this.handles.push ( mx.data.subscribe({
                 attr: this.attrStyle,
                 callback: ( guid, attr, badgeValue ) => this.updateRendering(),
                 guid: this.contextObject.getGuid()
             })
-            );
+            ); // The indent is wrong. It can move this parentheses one line up. // No need for white line... 
 
             this.handles.push( mx.data.subscribe ({
                 attr: this.attrLabel,
                 callback: ( guid, attr, badgeValue ) => this.updateRendering(),
                 guid: this.contextObject.getGuid()
             })
-            );
+            );  // The indent is wrong. It can move this parentheses one line up.
         }
     }
 }
 
+// Missing comments why this code is here?
+// let dojoBootstrapBadge is not used, so remove it before the statement dojoDeclare()
 const dojoBootstrapBadge = dojoDeclare(
     "com.mendix.widget.BootstrapBadge.BootstrapBadge",
     [ WidgetBase ],
     // tslint:disable-next-line:only-arrow-functions
-    (function (Source: any) {
+    (function (Source: any) { // no space between function and (
         let result: any = {};
         for (let i in Source.prototype) {
             if (i !== "constructor" && Source.prototype.hasOwnProperty(i)) {
@@ -122,5 +130,5 @@ const dojoBootstrapBadge = dojoDeclare(
             }
         }
         return result;
-    } (BootstrapBadge))
+    } (BootstrapBadge)) // no space between } and (
 );
