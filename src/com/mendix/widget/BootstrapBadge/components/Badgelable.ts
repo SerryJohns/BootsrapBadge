de0ac1541efb3e58c76fda7fcd914e3d9277a155
McDoyen <mrpoloh@yahoo.co.uk>
2016-12-08 13:35:24 +0300

    Use classNames for ternary operator in Badgelable component

--- src/com/mendix/widget/BootstrapBadge/components/Badgelable.ts
+++ src/com/mendix/widget/BootstrapBadge/components/Badgelable.ts
@@ -1,22 +1,22 @@
+import * as classNames from "classnames";
 import { createElement } from "react";
 import { DOM } from "react";
 
-import { BadgeProps } from "./Badge";
+import { BadgeProps, BadgeType } from "./Badge";
 import { ButtonBadgeItem } from "./BadgeButton";
 
 export function BadgeComponent(props: BadgeProps) {
-    // Tenary operator should use className
-    // Missing generic widget class name "widget-"
-    const badgeClasses = props.badgeType === "label"
-        ? "label label-" + props.bootstrapStyle
-        : props.badgeType === "btn"
-        ? "btn btn-" + props.bootstrapStyle
-        : "badge label-" + props.bootstrapStyle;
+    const badgeClasses = (badgeType: BadgeType, bootstrapStyle: string) =>
+        classNames( "widget-badge", {
+            [`label label-${bootstrapStyle}`] : badgeType === "label",
+            [`btn btn-${bootstrapStyle}`] : badgeType === "btn",
+            [`badge label-${bootstrapStyle}`] : badgeType === "badge"
+        });
 
     return createElement(props.badgeType === "btn" ? ButtonBadgeItem : BadgeItem, {
         MicroflowProps: props.MicroflowProps,
         badgeValue: props.badgeValue,
-        className: badgeClasses,
+        className: badgeClasses(props.badgeType, props.bootstrapStyle),
         label: props.label
     });
 }
@@ -32,20 +32,16 @@ export const BadgeItem = (props: BadgeProps) =>
     );
 
     const onClickMF = (props: BadgeProps) => {
-    // The used props are all optional.... so you should check them all.. and if the MicroflowProps really exist.
-    // WAS: if (props.MicroflowProps.microflow !== "") {
-    // No need to check on empty string, just check on true-sie value
-    // That will make the testing easier  
     if (props.MicroflowProps && props.MicroflowProps.microflow && props.MicroflowProps.guid) {
             window.mx.data.action({
-            error: (error: Error) => {
-                window.mx.ui.error(`Error while executing MicroFlow:
-                ${props.MicroflowProps.microflow}: ${error.message}`);
-            },
+                error: (error: Error) => {
+                    window.mx.ui.error(`Error while executing MicroFlow:
+                    ${props.MicroflowProps.microflow}: ${error.message}`);
+                },
                 params: {
                     actionname: props.MicroflowProps.microflow,
                     applyto: "selection",
-                    guids: [ props.MicroflowProps.guid ],
+                    guids: [ props.MicroflowProps.guid ]
                 }
             });
     }
--- src/com/mendix/widget/BootstrapBadge/components/__tests__/Badge.spec.ts
+++ src/com/mendix/widget/BootstrapBadge/components/__tests__/Badge.spec.ts
@@ -18,29 +18,29 @@ describe("A badge button", () => {
   //   window.mx.data = MxDataMock.prototype;
   // });
   it("should be of a badge structure", () => {
-    const button = renderBadge({ badgeValue: "" });
-    // console.log(button.debug());
+    const button = renderBadge({ badgeValue: "0" });
+    console.log(button.debug());
     expect(button).toBeElement(
       // createElement(BadgeItem, {})
       DOM.button({
-        className: "widget-progressbar progress",
+        className: "btn btn-warning",
         onClick: jasmine.any(Function) as any,
         style: { width: null }
       })
     );
   });
 
-  it("contains spec with an expectation", () => {
-    // 
-  });
-});
+//   it("contains spec with an expectation", () => {
+//     // 
+//   });
+// });
 
-describe("A badge label", () => {
-  it("contains spec with an expectation", () => {
-    // 
-  });
+// describe("A badge label", () => {
+//   it("contains spec with an expectation", () => {
+//     // 
+//   });
 
-  it("contains spec with an expectation", () => {
-    // 
-  });
-});
+//   it("contains spec with an expectation", () => {
+//     // 
+//   });
+ });
