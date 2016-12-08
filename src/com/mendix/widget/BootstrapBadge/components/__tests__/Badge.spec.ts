5b8ac255c12f25e13bf50020ba54b8bcea286717
McDoyen <mrpoloh@yahoo.co.uk>
2016-12-08 11:09:56 +0300

    Update karma.config.js file

--- src/com/mendix/widget/BootstrapBadge/components/__tests__/Badge.spec.ts
+++ src/com/mendix/widget/BootstrapBadge/components/__tests__/Badge.spec.ts
@@ -4,14 +4,29 @@ import { DOM, createElement } from "react";
 import { BadgeProps } from "../Badge";
 import { BadgeComponent, BadgeItem } from "../Badgelable";
 
+import { MxDataMock, MxLogger, MxMock, MxUiMock } from "../../../../../../../tests/mocks/Mendix";
+
 describe("A badge button", () => {
   const renderBadge = (props: BadgeProps) => shallow(createElement(BadgeComponent, props));
+  // let mxOriginal: mx.mx;
+
+  // beforeAll(() => {
+  //   mxOriginal = window.mx;
+  //   window.logger = MxLogger.prototype;
+  //   window.mx = MxMock.prototype;
+  //   window.mx.ui = MxUiMock.prototype;
+  //   window.mx.data = MxDataMock.prototype;
+  // });
   it("should be of a badge structure", () => {
     const button = renderBadge({ badgeValue: "" });
-    console.log(button.debug());
+    // console.log(button.debug());
     expect(button).toBeElement(
-      createElement(BadgeItem, {})
-      // DOM.button({})
+      // createElement(BadgeItem, {})
+      DOM.button({
+        className: "widget-progressbar progress",
+        onClick: jasmine.any(Function) as any,
+        style: { width: null }
+      })
     );
   });
 
@@ -20,12 +35,12 @@ describe("A badge button", () => {
   });
 });
 
-// describe("A badge label", () => {
-//   it("contains spec with an expectation", () => {
-//     // 
-//   });
+describe("A badge label", () => {
+  it("contains spec with an expectation", () => {
+    // 
+  });
 
-//   it("contains spec with an expectation", () => {
-//     // 
-//   });
-// });
+  it("contains spec with an expectation", () => {
+    // 
+  });
+});
